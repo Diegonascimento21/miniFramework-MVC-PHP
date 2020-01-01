509ec0982b0d23e5f8c7fdf177d67ef5e75b881e
diego nascimento <diegonasc21@gmail.com>
2020-01-01 17:48:19 -0200

    include layout

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -6,15 +6,15 @@ use stdClass;
 use MF\Controller\action;
 
 Class indexController extends Action{
-    
+
         public function index(){
             $this->view->dados = Array('cavalo', 'capivara', 'capote');
-            $this->render('index');
+            $this->render('index', 'layout1');
         }
 
         public function sobreNos(){
             $this->view->dados = Array('prisma', "somalia");
-            $this->render('sobreNos');
+            $this->render('sobreNos', 'layout1');
         } 
 
     }
--- vendor/MF/Controller/action.php
+++ vendor/MF/Controller/action.php
@@ -11,14 +11,24 @@ abstract class Action{
             $this->view = new \stdClass();
     }
 
-    protected function render($view){ //function para renderizar view
+    protected function render($view, $layout){ //function para renderizar o layout
+        $this->view->page = $view;
+
+        if(file_exists(require_once "../App/Views/".$layout.".phtml")){ //verificando se o layout existe 
+            require_once "../App/Views/".$layout.".phtml";              //fazendo o include
+        } else {
+            $this->Content();
+        }
+
+    }
+
+    protected function Content(){
         $classAtual = get_class($this); // metodos para pegar o caminho dinamicamente
         $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
         $classAtual = str_replace('Controller', '', $classAtual);
 
-        require_once "../App/Views/".$classAtual."/".$view.".phtml"; //partindo de public /views
+        require_once "../App/Views/".$classAtual."/".$this->view->page.".phtml"; //partindo de public /views
     }
-
 
 }
 ?>
