2bc3137a49d365d39561c8d481b09b3ab69eb44f
diego nascimento <diegonasc21@gmail.com>
2020-01-01 17:55:30 -0200

    connection bd

--- App/Connection.php
+++ App/Connection.php
@@ -4,6 +4,22 @@ namespace App;
 
 class Connection{
 
+    public function getdb(){
+        try{
+
+            $conn = new PDO(
+                "mysql:host=localhost;dbname=mvc;charset=utf8",
+                "root",
+                ""
+            );
+
+            return $conn;
+
+        } catch () {
+
+        }
+    }
+
 }
 
 
