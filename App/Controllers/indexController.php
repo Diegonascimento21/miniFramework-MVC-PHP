e1fae2a473b6ca721ea7e5ad7af8f94961f3881b
diego nascimento <diegonasc21@gmail.com>
2020-01-02 16:52:45 -0200

    create models and listing products

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -4,12 +4,26 @@ namespace App\Controllers;
 
 use stdClass;
 use MF\Controller\action;
+use App\Connection;
+use App\Models\Produto;
 
 Class indexController extends Action{
 
         public function index(){
-            $this->view->dados = Array('cavalo', 'capivara', 'capote');
-            $this->render('index', 'layout1');
+            //$this->view->dados = Array('cavalo', 'capivara', 'capote');
+
+            //instancia de conexao e usar funcao usando metodo estatico
+            $conn = Connection::getdb();
+
+            //instanciar modelo
+            $produto = new Produto($conn);
+
+            $produtos = $produto->getProdutos();
+
+            $this->view->dados = $produtos;
+            
+
+            $this->render('index', 'layout1'); //renderizar view e layout
         }
 
         public function sobreNos(){
